145fbc7de62c9f8e0eb9fa88255420d225c56460
captaindeng12138 <captaindeng12138@gmail.com>
2024-06-25 21:21:00 +0100

    test

--- src/main/java/cn/dcy/jsapi/controller/TestController.java
+++ src/main/java/cn/dcy/jsapi/controller/TestController.java
@@ -4,12 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /**
  * @author Kyle
  * @date 2024/06/08
  */
 @RestController
 public class TestController {
+
+    //@Resource(name = "testRedisTopic02")
+    //private RTopic redisTopicListener02;
+
+    //@RequestMapping(path = "/redistopic", method = RequestMethod.GET)
+    //void publicRedisTopic() {
+    //    redisTopicListener02.publish("Hello World!");
+    //}
 
     @RequestMapping(path = "/test", method = RequestMethod.GET)
     public String test() {
